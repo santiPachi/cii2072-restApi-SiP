1b558be8c8b9709f5d52f8eb00db9f7528686a07
Santi <santiagopacheco@MacBook-Pro-de-Santiago.local>
2020-12-02 11:31:42 -0500

    uplad proyect

---
+++ backend-pacheco/src/main/java/com/pacheco/backend/BackendPachecoApplication.java
@@ -0,0 +1,13 @@
+package com.pacheco.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPachecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPachecoApplication.class, args);
+	}
+
+}
---
+++ backend-pacheco/src/test/java/com/pacheco/backend/BackendPachecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pacheco.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPachecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
